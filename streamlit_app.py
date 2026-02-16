b1aa8e5ccebf36c72e027be9fae8400eda306ad3
Marcus Codrescu <95443477+MCodrescu@users.noreply.github.com>
2026-02-16 15:38:52 +0000

    update with login UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,25 @@ import streamlit as st
 
 st.title("Balcones")
 
-pages = {
-    "Timesheet": [
-        st.Page("pages/timesheet_upload.py", title="Upload")
-    ],
-    "Reports": [
-        st.Page("pages/reports.py", title="Reports")
-    ],
-}
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Microsoft", on_click=st.login)
 
-pg = st.navigation(pages, position="top")
-pg.run()
+if not st.user.is_logged_in:
+    login_screen()
+else:
+    pages = {
+        "Timesheet": [
+            st.Page("pages/timesheet_upload.py", title="Upload")
+        ],
+        "Reports": [
+            st.Page("pages/reports.py", title="Reports")
+        ],
+    }
+
+    pg = st.navigation(pages, position="top")
+    pg.run()
+
+st.button("Log out", on_click=st.logout)
+
